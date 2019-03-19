0dee2ad2dcc629277fa0f0d3b6786cef4cea0877
sean-espressoft <zx.chow@espressoft.com.my>
2019-03-19 17:18:56 +0800

    add SwipeCollectionView

---
+++ SwipeCollectionViewController/SwipeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SwipeCollectionViewCell.swift
+//  SwipeCollectionViewController
+//
+//  Created by Sean Espressoft on 14/03/2019.
+//  Copyright © 2019 Sean. All rights reserved.
+//
+
+import UIKit
+
+class SwipeCollectionViewCell: UICollectionViewCell {
+    
+}
